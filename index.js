f0f8e6169b3acdbe58f986efc5944b8c3d0e0661
Andrei <33538982+andreibesleaga@users.noreply.github.com>
2021-06-01 16:51:41 +0300

    part3 exercises 3.10-3.11

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -102,7 +103,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
